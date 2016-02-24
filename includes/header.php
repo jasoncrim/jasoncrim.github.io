3d15926b9d8a6717f86f2e0fb158154affaf48ca
Jason Crim <jasoncrim@msn.com>
2016-02-23 22:49:05 -0500

    added https://

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <title>Jason Crim</title>
-      <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,300,600,800" rel="stylesheet" type="text/css">
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,300,600,800" rel="stylesheet" type="text/css">
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
   		<link rel="stylesheet" href="css/reset.css">
       <link rel="stylesheet" href="css/simplegrid.css">
